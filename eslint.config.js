00c919e19b97c85393c64a7c86a09711bce3f5a6
Dmitry Bulankin <swed88@mail.ru>
2025-04-12 17:45:09 +0300

    tsconfig update

--- eslint.config.js
+++ eslint.config.js
@@ -4,33 +4,52 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import pluginQuery from '@tanstack/eslint-plugin-query'
+import prettier from 'eslint-plugin-prettier'
 
-export default tseslint.config(
+export default [
+  // Base config
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  // Ignore patterns
   { ignores: ['dist'] },
+
+  // TypeScript/React config
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module'
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.eslint.json'
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      query: pluginQuery
+      query: pluginQuery,
+      prettier: prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
-      'prettier/prettier': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 100,
+          singleQuote: true,
+          trailingComma: 'none',
+          bracketSpacing: true,
+          tabWidth: 2,
+          semi: false
+        }
+      ],
       ...pluginQuery.configs['flat/recommended'].rules
     }
   }
-)
+]
