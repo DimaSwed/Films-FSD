75ee34b3b45d59cb1d229476032be5bde6ec89b3
Dmitry Bulankin <swed88@mail.ru>
2025-04-19 14:10:05 +0300

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,62 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Films SPA',
+        short_name: 'Films',
+        description: 'Your favorite films in one app!',
+        theme_color: '#ffffff',
+        background_color: '#000000',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/web-app-manifest-192x192',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/films-fsd.vercel\.app\/.*$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 // 1 день
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
